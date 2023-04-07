8cca49b57c9e6e215198aa055c29337128352a88
Duong Dai Son <21010571@st.phenikaa-uni.edu.vn>
2023-04-07 11:48:48 +0700

    Chinh sua file book khach san

---
+++ HotelManagerment/src/main/java/Classes/Book.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DUONG DAI SON
+ */
+package Classes;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Book {
+    private UserInfor customer;
+    ArrayList<Room> rooms;
+}
